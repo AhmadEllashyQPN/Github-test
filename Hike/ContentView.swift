ba8f3f5ccb9cc079e143f7d97bfe7928a7aae50d
AhmedEllashy <ahmadellashy2000@gmail.com>
2024-10-31 20:32:31 +0300

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Ahmad Ellashy on 31/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ahmad Ellashy on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
